7d1654d974c94357cc094a81c835f282ee20f46d
Vince Kobe <cvincekobe@gmail.com>
2026-05-28 19:39:36 +0800

    fix: correct require paths

--- spots.php
+++ spots.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit(); }
 
-require '../config.php';
+require 'config.php';
 $stmt = $pdo->query("SELECT * FROM tourist_spots");
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
